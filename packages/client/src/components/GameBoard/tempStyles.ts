2bbd9d0f2a7a61ff96391083ad6942f87f3106e0
callmedim <8431947@gmail.com>
2024-03-18 04:24:53 +0300

    game component build

---
+++ packages/client/src/components/GameBoard/tempStyles.ts
@@ -0,0 +1,16 @@
+export const styles: Record<string, string> = {
+  display: 'flex',
+  flexDirection: 'column',
+
+  width: '100%',
+  height: '100%',
+  minHeight: '80vh',
+  background: '#cdc1b4',
+
+  color: '#776e65',
+  font: 'bold 40px Arial',
+  textBaseling: 'middle',
+
+  justifyContent: 'center',
+  alignItems: 'center',
+}
