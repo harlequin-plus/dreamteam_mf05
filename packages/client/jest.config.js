ac565b624aae85c9b579957a25b8702423ec2f5e
Denis Zheleznov <zhel1@yandex.ru>
2024-03-25 11:54:06 -0700

    tests fix - add transformer for svg files for jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -8,4 +8,8 @@ export default {
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.svg$": "<rootDir>/svgTransform.js"
+  },
 }
