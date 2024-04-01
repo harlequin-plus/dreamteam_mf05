2968ac03cd6915d79f4efae056dfeb108b669093
Mixamax <mixamax@inbox.ru>
2024-04-01 23:48:43 +0300

    добавил возврат на страницу с которой пришли после login

--- packages/client/src/components/LoginForm/index.tsx
+++ packages/client/src/components/LoginForm/index.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button'
 import { signIn } from '../../services/apiService'
 import { useAppDispatch } from '../../hooks/reduxTsHook'
 import { fetchUser } from '../../store/userState'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const userInitValue = {
   login: '',
@@ -23,6 +24,8 @@ export function LoginForm({ toggleShow }: Props) {
   const [user, setUser] = useState(userInitValue)
   const [errorValue, setErrorValue] = useState(errorInitValue)
   const dispatch = useAppDispatch()
+  const location = useLocation()
+  const navigate = useNavigate()
 
   const onChangeUserValue = (e: SyntheticEvent) => {
     const target = e.target as HTMLInputElement
@@ -49,6 +52,12 @@ export function LoginForm({ toggleShow }: Props) {
     dispatch(fetchUser())
     setUser(userInitValue)
     setErrorValue(errorInitValue)
+    // Возврат на страницу, с которой пришли
+    const back =
+      location.state?.back && location.state?.back !== location.pathname
+        ? location.state?.back
+        : '/'
+    navigate(back)
   }
 
   return (
