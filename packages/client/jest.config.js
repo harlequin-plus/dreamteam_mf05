267faf99f74d818da8cc1d027ffadc1782d50bce
Sergey Embulaev <embulaev@gmail.com>
2024-04-14 15:35:03 +0300

    DRE-46 фикс импорта dotenv для тестов

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+dotenv.config();
 
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
