062be3d9d81cd43c43c9012b300325d9bd1c0973
Denis Zheleznov <zhel1@yandex.ru>
2024-04-15 14:31:28 -0700

    Changed the way to earn points in the game.

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -3,6 +3,7 @@ import App from './App'
 import { render, screen } from '@testing-library/react'
 import { store } from './store'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
--- packages/client/src/components/GameBoard/GameBoard.tsx
+++ packages/client/src/components/GameBoard/GameBoard.tsx
@@ -1,85 +1,23 @@
-import React, { useEffect, useRef, useState, useMemo, useCallback } from 'react'
-import { GameEngine } from '../../utils/gameMoves'
-import { addNewTile, drawBoard, drawTiles } from '../../utils/gameDraw'
+import React, { useEffect, useRef, useState, useCallback } from 'react'
+import { GameEngine } from '../../utils/gameEngin'
 import { styles } from './tempStyles'
 import { FullscreenProvider } from '../FullscreenProvider'
 import { Button } from '@mui/material'
 import CloseFullscreenIcon from '../../assets/close_fullscreen.svg'
 import OpenFullscreenIcon from '../../assets/open_fullscreen.svg'
 
+const gameEngine = new GameEngine()
+
 const GameBoard: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null)
 
-  const [highScore, setScore] = useState<number>(2)
-  const [completed, setCompleted] = useState<boolean>(false)
+  const [highScore, setScore] = useState<number>(0)
 
-  const findLargestElement = useCallback(
-    (matrix: number[][]): number => {
-      let largestElement = Number.NEGATIVE_INFINITY
-
-      for (const row of matrix) {
-        for (const element of row) {
-          if (element > largestElement) {
-            largestElement = element
-          }
-        }
-      }
-
-      if (largestElement === 2048 && !completed) {
-        alert(
-          `Congrats! You are about to score 2048! Continue if you want bigger score`
-        )
-
-        setCompleted(true)
-      }
-      return largestElement
-    },
-    [completed]
-  )
-
-  const board = useMemo(
-    () => [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ],
-    []
-  )
-
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent) => {
-      let moved = false
-
-      switch (event.key) {
-        case 'ArrowUp':
-          moved = GameEngine.moveUp(board)
-          setScore(findLargestElement(board))
-          break
-        case 'ArrowDown':
-          moved = GameEngine.moveDown(board)
-          setScore(findLargestElement(board))
-          break
-        case 'ArrowLeft':
-          moved = GameEngine.moveLeft(board)
-          setScore(findLargestElement(board))
-          break
-        case 'ArrowRight':
-          moved = GameEngine.moveRight(board)
-          setScore(findLargestElement(board))
-          break
-        default:
-          break
-      }
-
-      if (moved) {
-        drawBoard(ctxRef, board)
-        addNewTile(ctxRef, board)
-      }
-    },
-    [board, findLargestElement]
-  )
+  const handleKeyDown = useCallback((event: KeyboardEvent) => {
+    gameEngine.onKeyboardPress(ctxRef, event)
+    setScore(gameEngine.score)
+  }, [])
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -88,9 +26,9 @@ const GameBoard: React.FC = () => {
 
       if (ctx) {
         ctxRef.current = ctx
-        drawBoard(ctxRef, board)
-        drawTiles(ctxRef, board)
-        addNewTile(ctxRef, board)
+        gameEngine.drawBoard(ctxRef)
+        gameEngine.drawTiles(ctxRef)
+        gameEngine.addNewTile(ctxRef)
         window.addEventListener('keydown', handleKeyDown)
       }
     }
@@ -98,7 +36,7 @@ const GameBoard: React.FC = () => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
     }
-  }, [board, handleKeyDown])
+  }, [handleKeyDown])
 
   const [isFullscreenEnabled, setFullscreenEnabled] = useState<boolean>(false)
   return (
