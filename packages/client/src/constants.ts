e532bcc3b2bc90b7080f34d89b83b7b3ccc50735
Mixamax <mixamax@inbox.ru>
2024-04-17 00:24:40 +0300

    change redirect URI

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,4 +1,4 @@
 export const baseURL = 'https://ya-praktikum.tech/api/v2'
 export const resourceURL = 'https://ya-praktikum.tech/api/v2/resources'
 export const dateLocale = 'ru-RU'
-export const oauthRedirectURI = 'http://localhost:3000'
+export const oauthRedirectURI = 'http://localhost'
