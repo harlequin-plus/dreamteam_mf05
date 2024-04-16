cad702e806061cde1c6bc663c17a87aa49622790
Mixamax <mixamax@inbox.ru>
2024-04-16 23:24:44 +0300

    added ya oauth

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,3 +1,4 @@
 export const baseURL = 'https://ya-praktikum.tech/api/v2'
 export const resourceURL = 'https://ya-praktikum.tech/api/v2/resources'
 export const dateLocale = 'ru-RU'
+export const oauthRedirectURI = 'http://localhost:3000'
