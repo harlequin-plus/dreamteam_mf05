7a8d20bd0e80c41331ed37766380123d5963e508
Denis Zheleznov <zhel1@yandex.ru>
2024-04-24 10:46:19 -0700

    Type fix

--- packages/client/src/models/TUser.ts
+++ packages/client/src/models/TUser.ts
@@ -2,10 +2,10 @@ export type TUserDTO = {
   id: number
   first_name: string
   second_name: string
-  display_name: string | undefined
+  display_name: string | null
   phone: string
   login: string
-  avatar: string | undefined
+  avatar: string | null
   email: string
   password: string
 }
