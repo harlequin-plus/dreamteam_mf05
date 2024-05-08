fd0c18a6ef78087dea8360620f6f67ff2122a8c8
PalHair <palad.ru@mail.ru>
2024-05-08 13:13:07 +0400

    add middleware

--- packages/client/src/pages/LeaderboardPage.tsx
+++ packages/client/src/pages/LeaderboardPage.tsx
@@ -1,14 +1,26 @@
 import { Leaderboard } from '../components/Leaderboard'
 import { Button } from '@mui/material'
-import axios from 'axios'
 
 function LeaderboardPage() {
   const Test = async () => {
-    const { data } = await axios.get('http://localhost:3001/topic', {
-      withCredentials: true,
-    })
-
-    // console.log(data)
+    try {
+      const res = await fetch('http://localhost:3001/topic', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+          title: 'asdfa',
+          comment: 'adf',
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      const data = await res.json()
+      console.log(data)
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(e.message)
+      }
+    }
   }
   return (
     <>
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,51 +1,50 @@
 import dotenv from 'dotenv'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import axios from 'axios'
+// import cors from 'cors'
 dotenv.config()
 
 import express from 'express'
 import { dbConnect } from './init'
 import { TopicRouter } from './routes/TopicRouter'
+import { getUserFromApi } from './api/auth'
+import { createUserInDB, getUserByIdFromDB } from './services/users'
 
 dbConnect()
 const app = express()
-function processCors(
+
+const processCors = (
   _req: any,
   res: { header: (arg0: string, arg1: string) => void },
   next: () => void
-) {
+) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Methods', 'GET')
+
   next()
 }
-
-app.use(cors())
+// app.use(cors())
 app.use(processCors)
 
-app.use('*', cookieParser())
-
 app.use('', async (req, res, next) => {
-  console.log('middleware')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Methods', 'POST')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  console.log('midllware')
   try {
-    const { data } = await axios.get(
-      `https://ya-praktikum.tech/api/v2/auth/user`,
-      {
-        headers: {
-          cookie: req.headers['cookie'],
-        },
-      }
-    )
-    console.log(data)
+    const user = await getUserFromApi(req)
+    const userDB = await getUserByIdFromDB(user.id)
+    if (!userDB) {
+      await createUserInDB(user)
+    }
+    res.status(200)
     next()
   } catch (e) {
-    res.status(403).send({ reason: 'afasdf' })
-    console.log('unaautor')
+    res.status(403).send({ reason: 'Forbidden' })
+    return
   }
 })
 
