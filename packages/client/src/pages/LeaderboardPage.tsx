b068a215e1efc6f8f5080d1f81a59501c556dbd0
palhair <palad.ru@mail.ru>
2024-05-09 18:25:09 +0400

    delete test button

--- packages/client/src/pages/LeaderboardPage.tsx
+++ packages/client/src/pages/LeaderboardPage.tsx
@@ -1,30 +1,8 @@
 import { Leaderboard } from '../components/Leaderboard'
-import { Button } from '@mui/material'
 
 function LeaderboardPage() {
-  const Test = async () => {
-    try {
-      const res = await fetch('http://localhost:3001/forum/reply', {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        body: JSON.stringify({
-          commentId: 6,
-          content: 'Edit Reply from front',
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      })
-      const data = await res.json()
-      console.log(data)
-    } catch (e) {
-      if (e instanceof Error) {
-        console.log(e.message)
-      }
-    }
-  }
   return (
     <>
-      <Button onClick={Test}>Test</Button>
       <Leaderboard />
     </>
   )
